72ce1045c55b31f6494fb6b27c6be6f105a806fb
Andrew Stiles <ndrwstls544@gmail.com>
2019-12-29 23:35:09 -0500

    removed unique true in mongoose

--- models/Chord.js
+++ models/Chord.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ChordSchema = new Schema({
     author: {type: String, required: true},
     chordName: {type: String, required: true},
-    currentNotes: {type: Array, required: true, unique: true},
+    currentNotes: {type: Array, required: true},
     type: {type: String, required: true} 
 });
 
