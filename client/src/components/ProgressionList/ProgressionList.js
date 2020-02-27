2066e53efb1b1f1b6686ae048d3f0d8fef1a3cab
Andrew Stiles <ndrwstls544@gmail.com>
2020-02-27 01:05:26 -0500

    added styling

--- client/src/components/ProgressionList/ProgressionList.js
+++ client/src/components/ProgressionList/ProgressionList.js
@@ -8,7 +8,7 @@ const ProgressionList = props => {
     console.log(props)
     const progressions = props.progressions.map(progression => {
         return (
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', background: 'white', margin: 20, padding: 20}}>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', background: 'white', margin: 20, padding: 20, borderRadius: 10, boxShadow: '0px 0px 15px 2px rgba(89,86,89,1)'}}>
                 <PlayButton activeChords={progression.currentChords} />
                 <h3>{progression.title}</h3>
                 <p>Added By: <strong>{progression.author}</strong></p>
