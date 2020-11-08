df83ffcdf9ec10b29d144a2aca0972f52b045b58
Andrew Stiles <ndrwstls544@gmail.com>
2020-11-08 16:06:02 -0500

    changed mongodb setup

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.promise = Promise
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/chord-storage", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost/chord-storage',
+	{
+	  useNewUrlParser: true,
+	  useUnifiedTopology: true,
+	  useCreateIndex: true,
+	  useFindAndModify: false
+	}
+);
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
