1a21572af44f13943be9cada44dc92529e3fde8a
Andrew Stiles <ndrwstls544@gmail.com>
2020-11-08 17:59:41 -0500

    yes

--- server.js
+++ server.js
@@ -48,11 +48,11 @@ app.use('/api/users', users)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get("/*", function(req, res) {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
